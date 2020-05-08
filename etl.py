d22a29930cefa73b340488a707f3af53e17c213f
Kallibek <45891802+Kallibek@users.noreply.github.com>
2020-05-07 22:04:44 -0700

    Update etl.py

--- etl.py
+++ etl.py
@@ -4,18 +4,31 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Copies user activity and music metadata from S3 to the tables of the Redshift database.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Inserts data from the two copied tables into other tables of the database.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    1. Create a connection to the existing Redshift database;
+    
+    2. Copies data from S3 to the tables of the Redshift database;
+    
+    3. Inserts data from the two tables into other tables of the database.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
