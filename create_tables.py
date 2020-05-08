feab7c5c8b1e59a50ed2797403efb97bdf372c16
Kallibek <45891802+Kallibek@users.noreply.github.com>
2020-05-07 22:05:07 -0700

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -4,18 +4,34 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    """
+    Deletes existing tables.
+    """
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    """
+    Creates new tables.
+    """
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    Creates a connection to the existing Redshift cluster;
+    
+    Deletes existing table in the database;
+    
+    Creates new tables;
+    
+    Closes the connection.
+        
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
